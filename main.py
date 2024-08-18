1e83c9660bcbb0de721119fb8811eb6d0c57b79a
sayantancodex <sayantanpatra68@gmail.com>
2024-08-18 21:24:13 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -19,23 +19,6 @@ print('''
 ''')
 
 print("WELCOME TO INFINITY FILE TRANSFER ARCHITECTURE")
-if platform.system() == "Windows":
-  console_output = subprocess.check_output("ncat -h",shell=True,text=True)
-  if ''' https://nmap.org/ncat ''' in console_output:
-      
-      print("[+] NETCAT INSTALLATION FOUND")
-  else:
-    print("[x] NO NETCAT INSTALLATION FOUND PLEASE INSTALL NETCAT FIRST!")
-    sys.exit()
-
-if platform.system() == "Linux":
-  console_output = subprocess.check_output("nc -h",shell=True,text=True)
-  if '''nc''' in console_output:
-      print("[+] NETCAT INSTALLATION FOUND")
-  else:
-    print("[x] NO NETCAT INSTALLATION FOUND PLEASE INSTALL NETCAT FIRST!")
-    sys.exit()
-   
 
 
 print("DO YOU WANT TO\n[1] SEND DATA\n[2] RECEIVE DATA\n")
